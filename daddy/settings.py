7f947835810d8291a8421ea607ca6bea2b8857fd
laudbrian <LAU@LAUs-MacBook-Air.local>
2013-12-11 09:59:20 -0800

    Created app and configured settings

--- daddy/settings.py
+++ daddy/settings.py
@@ -24,6 +24,20 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'templates')
+)
+
 ALLOWED_HOSTS = []
 
 
@@ -34,8 +48,11 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'daddy_app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,7 +75,7 @@ WSGI_APPLICATION = 'daddy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'database.db'),
     }
 }
 

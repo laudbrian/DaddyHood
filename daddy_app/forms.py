91162d7e98425b18f13f96e37f64506a60db10df
laudbrian <LAU@LAUs-MacBook-Air.local>
2013-12-11 11:20:29 -0800

    Created Forms

---
+++ daddy_app/forms.py
@@ -0,0 +1,65 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.utils.html import strip_tags
+from daddy_app.models import Note
+
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'Email'}))
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder': 'Last Name'}))
+    username = forms.CharField(widget=forms.widgets.TextInput(attrs={'placeholder': 'Username'}))
+    password1 = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password Confirmation'}))
+
+    def is_valid(self):
+        form = super(UserCreateForm, self).is_valid()
+        for f, error in self.errors.iteritems():
+            if f != '__all_':
+                self.fields[f].widget.attrs.update({'class': 'error', 'value': strip_tags(error)})
+        return form
+
+    class Meta:
+        fields = ['email', 'username', 'first_name', 'last_name', 'password1',
+                  'password2']
+        model = User
+
+
+class AuthenticateForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.widgets.TextInput(attrs={'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.widgets.PasswordInput(attrs={'placeholder': 'Password'}))
+
+    def is_valid(self):
+        form = super(AuthenticateForm, self).is_valid()
+        for f, error in self.errors.iteritems():
+            if f != '__all__':
+                self.fields[f].widget.attrs.update({'class': 'error', 'value': strip_tags(error)})
+        return form
+
+
+class NoteForm(forms.ModelForm):
+    content = forms.CharField(required=True, widget=forms.widgets.Textarea(attrs={'class': 'noteText'}))
+
+    def is_valid(self):
+        form = super(NoteForm, self).is_valid()
+        for f in self.errors.iterkeys():
+            if f != '__all__':
+                self.fields[f].widget.attrs.update({'class': 'error noteText'})
+        return form
+
+    class Meta:
+        model = Note
+        exclude = ('user',)
+
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
+
+    class Media:
+        css = {
+            'all': ('style.less',)
+        }

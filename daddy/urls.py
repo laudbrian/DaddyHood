38e1ce8429c6c479d2a3121634b7098ada8e97da
laudbrian <LAU@LAUs-MacBook-Air.local>
2013-12-11 13:07:49 -0800

    Implemented User Login and Sign Up, and in BASE_DIR changed os.path.dirname(os.path.dirname(__file__)) to os.path.dirname(os.path.abspath(__file__)), resynceddb and migrated to read template path

--- daddy/urls.py
+++ daddy/urls.py
@@ -5,8 +5,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'daddy.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'daddy_app.views.index'), # root
+    url(r'^login$', 'daddy_app.views.login_view'), # login
+    url(r'^logout$', 'daddy_app.views.logout_view'), # logout
+    url(r'^signup$', 'daddy_app.views.signup'), # signup
 
     url(r'^admin/', include(admin.site.urls)),
 )
